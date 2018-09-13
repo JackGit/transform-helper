45c283247601a3ebd05685e3e1535d79daa838fb
jackgit <ygjack414@hotmail.com>
2018-09-13 23:07:30 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,69 @@
+/*
+<div class="th-helper">
+  <div class="th-content"></div>
+  
+  <div class="th-tline"></div>
+  <div class="th-rline"></div>
+  <div class="th-bline"></div>
+  <div class="th-lline"></div>
+
+  <div class="th-tlpoint"></div>
+  <div class="th-tmpoint"></div>
+  <div class="th-trpoint"></div>
+  <div class="th-brpoint"></div>
+  <div class="th-bmpoint"></div>
+  <div class="th-blpoint"></div>
+  <div class="th-rmpoint"></div>
+  <div class="th-lmpoint"></div>
+
+  <div class="th-box"></div>
+</div>
+*/
+
+const transformHelper = new TransformHelper({
+  classPrefix: 'th-helper',
+  renderer: {
+    bottomRightResizeHandle: () => {},
+    rotateHandle: () => {},
+    boxHandle: () => {}
+  }
+})
+
+transformHelper.wrap(el, {
+  rotate: true,
+  scale: true, // resize
+  move: true
+})
+
+transformHelper.on('', () => {})
+
+// getter & setter
+
+// rotation
+transformHelper.rotate.deg
+transformHelper.rotate.origin = { x, y }
+
+transformHelper.rotateTo(deg)
+transformHelper.rotateBy(deg)
+
+// size
+transformHelper.keepRatio
+transformHelper.scale = { x, y }
+transformHelper.width
+transformHelper.height
+
+transformHelper.scale(x, y)
+transformHelper.scaleX(x, keepRatio)
+transformHelper.scaleY(y, keepRatio)
+
+// position
+transformHelper.x
+transformHelper.y
+
+transformHelper.moveTo()
+transformHelper.moveBy()
+
+
+transformHelper.enableRotate()
+transformHelper.disableRotate()
+transformHelper.rotateEnabled
